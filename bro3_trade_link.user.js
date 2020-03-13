6b6ca5f88240a575f0a52c26f9030f05fe46d9aa
kuroame <haa85443@gmail.com>
2020-03-13 09:51:11 +0900

    Update bro3_trade_link.user.js

--- bro3_trade_link.user.js
+++ bro3_trade_link.user.js
@@ -3,7 +3,7 @@
 // @namespace      ブラウザ三国志で出品カードのトレードリンクを表示する
 // @include        http://*.3gokushi.jp/card/exhibit_list.php*
 // @include        http://*.3gokushi.jp/card/bid_list.php*
-// @icon           https://raw.github.com/kuroame3/bro3_trade_link/8950.png
+// @icon           https://raw.githubusercontent.com/kuroame3/bro3_trade_link/master/8950.png
 // @description    出品カードのトレードリンク表示
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js
 // @version	   0.1.0
